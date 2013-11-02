aa2158b395fad9c1c3007c525f2742d2cbdf0762
Wisen Tanasa <wisen@ceilfors.com>
2013-11-02 18:05:41 +0800

    Change url

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -7,8 +7,8 @@
 // In this case it is a simple value service.
 var services = angular.module('carpoolBuddyApp.services', ['ngResource']).
 factory('cityService', function($resource) {
-    return $resource('/rest/cities', {}, {}); 
+    return $resource('/cities', {}, {}); 
 }).
 factory('buddyService', function($resource) {
-    return $resource('/rest/buddies', {}, {});
+    return $resource('/buddies', {}, {});
 })
