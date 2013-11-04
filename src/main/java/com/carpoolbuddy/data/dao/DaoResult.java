7ae3030396ae009d20afed9129ce8d7c54c73093
edjayks <edjayks@gmail.com>
2013-11-04 15:15:21 +0800

    Add DaoResult back in

---
+++ src/main/java/com/carpoolbuddy/data/dao/DaoResult.java
@@ -0,0 +1,14 @@
+package com.carpoolbuddy.data.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexus
+ * Date: 11/4/13
+ * Time: 3:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum DaoResult {
+    RECORD_ALREADY_EXIST,
+    RECORD_CREATED_SUCCESSFULLY,
+    RECORD_CREATION_FAILED
+}
